2e97ce51b4264d8fac1ab53e657b33493b8511de
Thomas Kopp <thom_ko@web.de>
2018-11-13 09:39:40 +0400

    Scripts: Image inserter
    Inserting the thumbnails into the database
    
    Signed-off-by: Thomas Kopp <thom_ko@web.de>

--- scripts/image-inserter.py
+++ scripts/image-inserter.py
@@ -22,26 +22,27 @@ os.chdir(imageFolder)
 transformStr = str(thumbDimension) + 'x' + str(thumbDimension)
 
 #Queries
-selectQuery = 'SELECT rowid, ' + imagePathColumn + ' FROM ' + table + ' LIMIT 100,5'
-updateQuery = 'UPDATE ' + table + ' SET ' + imageThumbColumn + ' = "HalloWelt" WHERE rowid = ?'
+selectQuery = 'SELECT rowid, ' + imagePathColumn + ' FROM ' + table
+updateQuery = 'UPDATE ' + table + ' SET ' + imageThumbColumn + ' = ? WHERE rowid = ?'
 
 conn = sqlite3.connect(dbFile)
-for row in conn.execute(selectQuery):
+c = conn.cursor()
+c.execute(selectQuery)
+rows = c.fetchall()
+for row in rows:
     imageFile = row[1].replace('\\','/')
     img = PythonMagick.Image(imageFile)
     if max(img.size().height(), img.size().width()) > thumbDimension :
         img.transform(transformStr)
     blob = PythonMagick.Blob()
+    img.quality(30)
     img.write(blob, "jpg")
     try:
         blobData = blob.data
     except UnicodeDecodeError as e:
         blobData = e.object
 
-    print (updateQuery)
-    print (row[0])
-    blobData = "HalloWelt"
-    #conn.execute(updateQuery, (blobData, row[0]))
-    conn.execute(updateQuery, (row[0]))
+    c.execute(updateQuery, (blobData, row[0]))
 
+conn.commit()
 conn.close()
